2ab606630c1b75196ab4ad29a39b1444e995df68
Meng Chen <meng.chen03@gmail.com>
2020-02-07 11:55:22 -0800

    revise the codes and reorganize the files

--- app.py
+++ app.py
@@ -1,68 +1,156 @@
-
-from refinder import *
+#
 import streamlit as st
+import pandas as pd
+import geopandas as gpd
 import folium
-from sklearn.linear_model import LogisticRegression
 import pickle
-from sklearn.preprocessing import StandardScaler
+import numpy as np
+from itertools import chain
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import FunctionTransformer
 
-# from sqlalchemy import create_engine
-# from sqlalchemy_utils import database_exists, create_database
-# import pandas as pd
-# import psycopg2
-
 # Model saved in pickle
-DATA_hmda_acs = 'hmda_acs_merged.csv'
-DATA_zip_tract = 'zip_tract_122017.xlsx'
-DATA_shp = '2019/tl_2019_53_tract.shp'
-MODEL_path = 'finalized_model.sav'
+DATA_hmda_acs = 'data/hmda_acs_merged.csv'
+DATA_zip_tract = 'data/zip_tract_122017.xlsx'
+DATA_shp = 'data/2019/tl_2019_53_tract.shp'
+DATA_zipcodes = 'data/zipcodes_king.csv'
+MODEL_lr_nh = 'data/lr_model_nh.sav'
+MODEL_lr_hf = 'data/lr_model_hf.sav'
+MODEL_rf_nh = 'data/rf_model_nh.sav'
+MODEL_rf_hf = 'data/rf_model_hf.sav'
 
-#sscaler = StandardScaler()
+model_dict = {'Logistic Regression': {'Non-human': MODEL_lr_nh,
+                                      'Human-first': MODEL_lr_hf},
+              'Random Forest': {'Non-human': MODEL_rf_nh,
+                                'Human-first': MODEL_rf_hf}
+            }
+
 transformer = FunctionTransformer(np.log1p, validate=True)
-model = pickle.load(open(MODEL_path, 'rb'))
+scaler = MinMaxScaler(feature_range=(0.2, 0.8))
+
+st.title('intelliRefinder')
+st.markdown('''Finding refinance opportunities for mortgage lenders using
+               machine learning algorithms.
+            ''')
+
+st.markdown('''The hundreds of GBs data of the
+               projects were collected via Home Mortgage Disclosure Act and
+               Census American Community Survey.
+            ''')
+
+zipcodes = pd.read_csv(DATA_zipcodes)['zip']
+algorithms = ('Logistic Regression', 'Random Forest')
+interventions = ('Non-human', 'Human-first')
+
+st.sidebar.markdown('Options')
+zipcode = st.sidebar.selectbox('Please select zip code', zipcodes)
+algo = st.sidebar.selectbox('Select algorithm', algorithms)
+itvn = st.sidebar.selectbox('Intervention',interventions)
 
 @st.cache(persist=True, suppress_st_warning=True)
-def get_tract3(zipcode, file=DATA_zip_tract):
+def zip2tract(state_code=53):
+    z2t = {}
+    zt = []
+    df = pd.read_excel(DATA_zip_tract)
+    df.columnns = ['zip', 'tract']
+    for z, t in zip(df['zip'], df['tract']):
+        if str(t)[:2] == str(state_code):
+            zt.append([z, str(t)[-5:]])
+        else:
+            continue
+    zt = pd.DataFrame(zt, columns=['zip', 'tract'])
+    # if you tract number is 5 digits, you don't need to /100
+    # however, you might want to change the tract from str to int
+    zt['tract'] = zt['tract'].astype(float)/100
+    z_unique =  zt['zip'].unique()
+    for _, row in zt.iterrows():
+        ts = zt[zt['zip'] == row[0]]
+        z2t.update({row[0]: ts['tract']})
+    return z2t
+
+def get_tract(zipcode):
     zipcode = int(zipcode)
-    results = zip2tract(file, 53)[zipcode]
-    return results
+    return zip2tract()[zipcode]
+
+def select_model(algo, itvn):
+    return model_dict[algo][itvn]
 
 @st.cache(persist=True, suppress_st_warning=True)
-def model_predict(zipcode):
-    tracts = get_tract3(zipcode)
+def load_model(algo, itvn):
+    model_path = select_model(algo, itvn)
+    return pickle.load(open(model_path, 'rb'))
+
+@st.cache(persist=True, suppress_st_warning=True)
+def load_data(itvn):
     df = pd.read_csv(DATA_hmda_acs)
+    ethical_related = df.columns[41:80]
+    if itvn == 'Non-human':
+        df.drop(['loan_purpose_1', 'loan_purpose_2', 'loan_purpose_3'],
+                axis=1,inplace=True)
+    elif itvn == 'Human-first':
+        df.drop(ethical_related, axis=1, inplace=True)
     df.rename(columns={'Unnamed: 0': 'census_tract_number'}, inplace=True)
-    idxs = df['census_tract_number'].isin(tracts.values)
-    tracts = df[idxs].iloc[:, 0]
-    df = df[idxs].iloc[:, 1:-2]
-    x = transformer.transform(df)
+    return df
+
+def predict(tracts, algo, itvn):
+    df = load_data(itvn)
+    df_x = df.iloc[:, :-2]
+    idxs = df_x['census_tract_number'].isin(tracts.values)
+    tracts = df_x[idxs].iloc[:, 0]
+    df_x = df_x[idxs].iloc[:, 1:]
+    scores = df[idxs].iloc[:, -2]
+    x = transformer.transform(df_x)
+    model = load_model(algo, itvn)
     results = model.predict(x)
+    results = results + scores
+    results = scaler.fit_transform(np.asarray(results).reshape(-1,1))
+    results = [i for i in chain(*results)]
     tracts = np.asarray(tracts)
     results_merged = pd.DataFrame({'tracts': tracts, 'results': results})
     results_merged.rename(columns={'tracts':'census_tract_number',
                                    'results':'Refinance_score'}, inplace=True)
     return results_merged
 
+@st.cache(persist=True, suppress_st_warning=True)
+def get_geodata(shp):
+    gdf = gpd.read_file(shp).to_crs({'init':'epsg:4326'})
+    gdf['TRACTCE'] = gdf['TRACTCE'].astype(float)/100
+    gdf.rename(columns={'TRACTCE': 'census_tract'}, inplace=True)
+    gdf['census_tract'] = gdf['census_tract'].astype(float)
+    return gdf
+
+def map_plot(geo_data, data):
+    lat = geo_data['INTPTLAT'].astype(float).mean()
+    lon = geo_data['INTPTLON'].astype(float).mean()
+    plot = folium.Map([lat, lon],
+                      zoom_start=9)
+    plot.choropleth(geo_data=geo_data,
+                    name='choropleth',
+                    data=data,
+                    columns=['census_tract_number', 'Refinance_score'],
+                    key_on='feature.properties.census_tract',
+                    fill_color='YlOrRd',
+                    na_fill_color='white',
+                    na_fill_opacity=0.2,
+                    fill_opacity=0.7,
+                    line_weight=0.6,
+                    line_opacity=0.2)
+    return plot
 
 def main():
-    st.title('Mortgage Refinance Opportunity')
-    st.write('')
-    zipcode = st.text_input(label='Input zip code, for example: 98105', value='98105')
-    st.text('STATUS: Gathering mortgage transcation and census informaton...')
-    tracts = get_tract3(zipcode)
-    st.text('STATUS: Engineering features...')
-    finance_scores = model_predict(zipcode)
-    st.text('STAUTS: Locating information base on local zip codes...')
+    tracts = get_tract(zipcode)
+    scores = predict(tracts, algo, itvn)
     gdf = get_geodata(DATA_shp)
     geodata = gdf.loc[gdf['census_tract'].isin(tracts.values)]
-    data = finance_scores.loc[finance_scores['census_tract_number']
-                              .isin(tracts.values)]
-    st.text('STATUS: Generating maps of mortgage refinance opportunities...')
-    map = map_plot([47.6062, -122.3321], geodata , data)
+    data = scores.loc[scores['census_tract_number'].isin(tracts.values)]
+    map = map_plot(geodata , data)
 
     return st.markdown(map._repr_html_(), unsafe_allow_html=True)
     #return None
 
-if __name__ == '__main__':
-    main()
+main()
+
+st.markdown('''If you have questions about or are interested in the project,
+               please feel free to contact me @meng.chen03(at)gmail.com.
+            ''')
